8795e89e51facc6d145fc22b2206a361a801bb3c
Juan <borquez.juan00@gmail.com>
2023-11-15 12:38:27 -0300

    mensaje de salida en server

--- cons_5/export/src/server/Server.cpp
+++ cons_5/export/src/server/Server.cpp
@@ -10,19 +10,15 @@ int main(int argc, char* argv[]) {
     std::cerr << "Uso: Server Puerto verbosity\n";
     return -1;
   }
+  std::cout << "Presione 'CTRL + C' para salir."<< std::endl;
 
   int port = atoi(argv[1]);
   int verbosity = atoi(argv[2]);
 
   C5Server myServer;
   XmlRpc::setVerbosity(verbosity);
-
   myServer.bindAndListen(port);
   myServer.enableIntrospection(true);
-  myServer.work(-1.0);
-  std::cout << "Presione 'q' para salir."<< std::endl;
-  
-  myServer.shutdown();
+  myServer.work(-10);
   return 0;
 }
-
