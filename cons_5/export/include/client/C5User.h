f47edab52831f3e769bf826add4367f362875864
Juan <borquez.juan00@gmail.com>
2023-11-15 01:42:48 -0300

    comments C5Users.h

--- cons_5/export/include/client/C5User.h
+++ cons_5/export/include/client/C5User.h
@@ -1,11 +1,35 @@
 #ifndef _USER_H
 #define _USER_H
+
 #include <string>
-class C5User{
-    public:
-        C5User(int _id): id(_id){}
-        int getId() const {return id;}
-    private:
-        int id;
+
+/**
+ * @brief Represents a user.
+ *
+ * This class stores information about a user.
+ */
+class C5User {
+public:
+    /**
+     * @brief Constructor for C5User.
+     *
+     * Initializes a C5User with the provided user ID.
+     *
+     * @param _id The ID of the user.
+     */
+    C5User(int _id) : id(_id) {}
+
+    /**
+     * @brief Gets the user ID.
+     *
+     * Retrieves the ID of the user.
+     *
+     * @return The ID of the user.
+     */
+    int getId() const { return id; }
+
+private:
+    int id; /**< The ID of the user. */
 };
+
 #endif
