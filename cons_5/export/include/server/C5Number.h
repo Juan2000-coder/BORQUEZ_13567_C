f1fdfd23799be09863d74918a36babfef7b905d1
Juan <borquez.juan00@gmail.com>
2023-11-15 09:24:34 -0300

    C5Number.h doc

--- cons_5/export/include/server/C5Number.h
+++ cons_5/export/include/server/C5Number.h
@@ -3,26 +3,83 @@
 
 #include <string>
 
+/**
+ * @brief Template class representing a numeric object.
+ * Type would be double or int for "real" or "entero" cases.
+ *
+ * @tparam Type The data type of the numeric value.
+ */
+
 template <class Type> class C5Number {
   public:
+    /**
+     * @brief Constructor for C5Number class.
+     *
+     * Initializes a C5Number instance with the provided bounds, ordinal, and timestamp.
+     *
+     * @param bmin Minimum bound for the numeric value.
+     * @param bmax Maximum bound for the numeric value.
+     * @param time Timestamp associated with the numeric value.
+     */
     C5Number(Type bmin, Type bmax, long time);
 
+    /**
+     * @brief Sets the ordinal value for the C5Number instance.
+     *
+     * @param ordinal The ordinal value to be set.
+     */
     void setOrdinal(int ordinal);
 
+    /**
+     * @brief Gets the ordinal value of the C5Number instance.
+     *
+     * @return The ordinal value.
+     */
     int getOrdinal() const;
+
+    /**
+     * @brief Gets the data type of the C5Number instance.
+     *
+     * @return The data type as a string.
+     */
     std::string getType() const;
+
+    /**
+     * @brief Gets the numeric value of the C5Number instance.
+     *
+     * @return The numeric value.
+     */
     Type getValue() const;
+
+    /**
+     * @brief Gets the minimum bound of the C5Number instance.
+     *
+     * @return The minimum bound.
+     */
     Type getBmin() const;
+
+    /**
+     * @brief Gets the maximum bound of the C5Number instance.
+     *
+     * @return The maximum bound.
+     */
     Type getBmax() const;
+
+    /**
+     * @brief Gets the timestamp associated with the instance.
+     *
+     * @return The timestamp.
+     */
     long getTime() const;
 
   private:
-    int ordinal;
-    std::string type;
-    Type value;
-    Type bmin;
-    Type bmax;
-    long time;
+    int ordinal;       ///< Ordinal value of the C5Number.
+    std::string type;  ///< Data type of the C5Number ("entero" o "real").
+    Type value;        ///< Numeric value of the C5Number.
+    Type bmin;         ///< Minimum bound of the C5Number.
+    Type bmax;         ///< Maximum bound of the C5Number.
+    long time;         ///< Timestamp associated with the C5Number.
 };
+
 #include "C5Number.t.hpp"
 #endif
