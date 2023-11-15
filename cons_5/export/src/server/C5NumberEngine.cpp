f3639b73a4b726f942cf82d879b259ff75d3df5a
Juan <borquez.juan00@gmail.com>
2023-11-15 10:30:38 -0300

    C5NumberEngine.cpp doc

--- cons_5/export/src/server/C5NumberEngine.cpp
+++ cons_5/export/src/server/C5NumberEngine.cpp
@@ -1,22 +1,25 @@
 #include "C5NumberEngine.h"
 #include <cstdlib>
 #include <ctime>
-#include <utility>//ver después si realmente hace falta su inclusión
-#include <iostream>
+#include <utility>
+#include <fstream>
 
 C5NumberEngine::C5NumberEngine(){
+    // Path relative to the buid folder.
     this->validUsersFile = "../files/users.txt";
 }
+
 bool C5NumberEngine::userValidate(int userId){
-    //Primero verifica en la lista de requerimientos
     int index = 0;
     for(C5Requirement &requirement: this->requirements){
+        // Checks if the user is already in the list of requirements.
         if (requirement.getUserId() == userId){
             this->requirementIndex = index;
-            return true;
+            return true; // User already in the list of requirements.
         }
         index++;
     }
+
     std::ifstream validUsers(this->validUsersFile);
     if (!validUsers.is_open()){
         throw C5NumberEngineException(C5NumberEngineException::exceptionCodes::AT_OPEN);
@@ -24,49 +27,51 @@ bool C5NumberEngine::userValidate(int userId){
     std::string idRequest = std::to_string(userId);
     std::string id;
 
-    while (std::getline(validUsers, id)){
-        if (id == idRequest){
-            /*Si es valido también creamos un request correspondiente*/
-            this->requirements.push_back(C5Requirement(userId));
+    while (std::getline(validUsers, id)){// Checks if the user is in the file.
+        if (id == idRequest){// Valid user.
+            this->requirements.push_back(C5Requirement(userId)); // Creates a new Requirement.
             this->requirementIndex = this->requirements.size() - 1;
             return true;
         }
     }
-    return false;
+    return false; // Not valid user.
 }
 
 
 int C5NumberEngine::getInt(int bmin, int bmax){
     auto &requirement = this->requirements.at(this->requirementIndex);
+
+    // Creates a new number and then returns it's value.
     requirement.appendNumber<int>(bmin, bmax);
     return requirement.getNumber<int>().getValue();
 }
 
 int C5NumberEngine::getInt(){
     auto &requirement = this->requirements.at(this->requirementIndex);
+
+    // Creates a new number and then returns it's value.
     requirement.appendNumber<int>();
     return requirement.getNumber<int>().getValue();
 }
 
 double C5NumberEngine::getReal(double bmin, double bmax){
-    //La verificación lanza una excepción en caso de que no se haya realizado.
     auto &requirement = this->requirements.at(this->requirementIndex);
+
+    // Creates a new number and then returns it's value.
     requirement.appendNumber<double>(bmin, bmax);
     return requirement.getNumber<double>().getValue();
 }
 double C5NumberEngine::getReal(){
-        //La verificación lanza una excepción en caso de que no se haya realizado.
     auto &requirement = this->requirements.at(this->requirementIndex);
+
+    // Creates a new number and then returns it's value.
     requirement.appendNumber<double>();
     return requirement.getNumber<double>().getValue();
 }
 Stats C5NumberEngine::getStat() const{
-    //La verificación lanza una excepción en caso de que no se haya realizado.
     auto requirement = this->requirements.at(this->requirementIndex);
     return requirement.getStats();
 }
-//A la mierda, devolvemos el vector de variants y a la chota.
 const C5Requirement& C5NumberEngine::getNumberList() const{
-    //La verificación lanza una excepción en caso de que no se haya realizado.
     return this->requirements.at(this->requirementIndex);
 }
