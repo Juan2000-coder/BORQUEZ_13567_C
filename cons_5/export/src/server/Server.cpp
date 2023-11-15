2d25d4e91364c5ca1083971f18f4383ffbb14ff5
Juan <borquez.juan00@gmail.com>
2023-11-15 14:10:57 -0300

    header of documenting in Server.cpp

--- cons_5/export/src/server/Server.cpp
+++ cons_5/export/src/server/Server.cpp
@@ -1,3 +1,22 @@
+/**
+ * @file    Server.cpp
+ * @brief   Main program of the XML-RPC Server.
+ * 
+ * @details
+ * This file contains the main implementation of the XML-RPC Server.
+ * 
+ * @calling
+ * Executable: ./server <port> <verbosity>
+ * 
+ * @description
+ * Sets up the server and puts it to work.
+ * 
+ * @version  1.0
+ * @date     2023-11-15
+ * @author   Juan Manuel Borquez
+ * @contact  borquez.juan00@gmail.com
+ */
+
 #include "XmlRpc.h"
 #include "C5Server.h"
 #include <iostream>
@@ -7,7 +26,7 @@ using namespace XmlRpc;
 
 int main(int argc, char* argv[]) {
   if (argc != 3) {
-    std::cerr << "Uso: Server Puerto verbosity\n";
+    std::cerr << "Uso: ./server <puerto> <verbosity>\n";
     return -1;
   }
   std::cout << "Presione 'CTRL + C' para salir."<< std::endl;
@@ -18,7 +37,9 @@ int main(int argc, char* argv[]) {
   C5Server myServer;
   XmlRpc::setVerbosity(verbosity);
   myServer.bindAndListen(port);
+  
+  // Enable introspection for help in the server methods.
   myServer.enableIntrospection(true);
-  myServer.work(-10);
+  myServer.work(-10);   // Listen to requirement for ever.
   return 0;
 }
