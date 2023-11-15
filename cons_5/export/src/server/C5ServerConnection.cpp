38fc2619d368d532caf5cd202200b4006f160b3f
Juan <borquez.juan00@gmail.com>
2023-11-15 10:50:42 -0300

    C5ServerConnection.cpp doc

--- cons_5/export/src/server/C5ServerConnection.cpp
+++ cons_5/export/src/server/C5ServerConnection.cpp
@@ -2,7 +2,6 @@
 #include "XmlRpcUtil.h"
 #include "XmlRpcException.h"
 #include "C5Exceptions.h"
-#include <iostream>
 
 using namespace XmlRpc;
 
@@ -11,19 +10,19 @@ void C5ServerConnection::executeRequest(){
     std::string methodName = parseRequest(params);
     XmlRpcUtil::log(2, "XmlRpcServerConnection::executeRequest: server calling method '%s'",
                     methodName.c_str());
-    try {
+    try { // In this block both XmlRpc library exception and custom exceptions are rise.
         if (! executeMethod(methodName, params, resultValue) &&
             ! executeMulticall(methodName, params, resultValue))
             generateFaultResponse(methodName + ": unknown method name");
         else
             generateResponse(resultValue.toXml());
     }
-    catch(const C5Exceptions &e){
-        std::cerr << e;
-        resultValue = e.what();
-        generateFaultResponse(e.what(), e.getCode());
+    catch(const C5Exceptions &e){  // Catches custom exceptions.
+        std::cerr << e;            // Outputs the exception object to cerr.
+        resultValue = e.what();    // Custom exception message.
+        generateFaultResponse(e.what(), e.getCode()); // Fault response.
     }
-    catch (const XmlRpcException& fault) {
+    catch (const XmlRpcException& fault) { // XmlRpc library exception.
         XmlRpcUtil::log(2, "XmlRpcServerConnection::executeRequest: fault %s.",
                         fault.getMessage().c_str()); 
         generateFaultResponse(fault.getMessage(), fault.getCode());
