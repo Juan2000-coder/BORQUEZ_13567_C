e7bd6eadffaa713af4952150b0a97a78d0c870bf
Juan <borquez.juan00@gmail.com>
2023-11-15 10:19:05 -0300

    C5ServerConnection.h

--- cons_5/export/include/server/C5ServerConnection.h
+++ cons_5/export/include/server/C5ServerConnection.h
@@ -1,10 +1,24 @@
 #ifndef _C5SERVERCONNECTION_H
 #define _C5SERVERCONNECTION_H
+
 #include "XmlRpcServerConnection.h"
 
 using namespace XmlRpc;
-class C5ServerConnection: public XmlRpcServerConnection{
-    using XmlRpcServerConnection::XmlRpcServerConnection;
+
+/**
+ * @brief Custom server connection class.
+ *
+ * This class extends XmlRpcServerConnection to overrides the executeRequest method.
+ */
+class C5ServerConnection : public XmlRpcServerConnection {
+    using XmlRpcServerConnection::XmlRpcServerConnection; // Use Base class constructors.
+
+    /**
+     * @brief Overrides the execution of an XML-RPC request.
+     *
+     * It overrides the base class method to provide custom exception
+     * handling specific to C5.
+     */
     void executeRequest() override;
 };
 
