1da1b57c9359e793f9eee5d6c6fe6ca82e253785
Juan <borquez.juan00@gmail.com>
2023-11-15 01:55:22 -0300

    C5Exception.h documentation

--- cons_5/export/include/server/C5Exceptions.h
+++ cons_5/export/include/server/C5Exceptions.h
@@ -6,48 +6,83 @@
 #include <iostream>
 #include <sstream>
 
-class C5Exceptions: public std::exception{
+/**
+ * @brief Base class for C5 exceptions, derived from std::exception.
+ */
+class C5Exceptions: public std::exception {
   public:
-    enum exceptionCodes{NOID};
+    enum exceptionCodes { NOID };//(NOID) No Identified
+
     C5Exceptions(const char* _message): message(_message), code(static_cast<unsigned int>(NOID)){}
-    C5Exceptions():message("No dentificada"), code(static_cast<unsigned int>(NOID)){}
-    const char* what() const noexcept override{
+    C5Exceptions(): message("No identified"), code(static_cast<unsigned int>(NOID)){}
+
+    const char* what() const noexcept override {
       return message;
     }
-    virtual const char* where() const{return "base";}
-    unsigned int getCode() const {return this->code;}
+
+    /**
+     * @brief Virtual method to provide information about the exception's origin.
+     * @return A string indicating the origin of the exception.
+     */
+    virtual const char* where() const { return "base"; }
+
+    /**
+     * @brief Getter method to retrieve the exception code.
+     * @return The exception code.
+     */
+    unsigned int getCode() const { return this->code; }
+
+    /**
+     * @brief Friend function to overload the output stream operator for printing exceptions.
+     * @param os The output stream.
+     * @param myExceptions The C5Exceptions object to be printed.
+     * @return The output stream.
+     */
     friend std::ostream& operator<<(std::ostream& os, const C5Exceptions& myExceptions);
 
   protected:
-    const char* message;
-    unsigned int code;
+    const char* message; // Exception message.
+    unsigned int code;   // Exception code.
 };
 
-class C5NumberException: public C5Exceptions{
+/**
+ * @brief Derived class for C5 number-related exceptions.
+ */
+class C5NumberException: public C5Exceptions {
   public:
-    enum exceptionCodes {LIMIT, TYPE};
+    enum exceptionCodes { LIMIT, TYPE };
     C5NumberException(exceptionCodes code);
-    const char* where() const override {return "C5Number";}
+    const char* where() const override { return "C5Number"; }
 };
 
-class C5RequirementException: public C5Exceptions{
+/**
+ * @brief Derived class for C5 requirement-related exceptions.
+ */
+class C5RequirementException: public C5Exceptions {
   public:
-    enum exceptionCodes {OUT_OF_RNG, NO_PREV_REQ, NO_PREV_REQ_TYPE};
+    enum exceptionCodes { OUT_OF_RNG, NO_PREV_REQ, NO_PREV_REQ_TYPE };
     C5RequirementException(exceptionCodes code);
-    const char* where() const override {return "C5Requirement";}
+    const char* where() const override { return "C5Requirement"; }
 };
 
-class C5NumberEngineException: public C5Exceptions{
+/**
+ * @brief Derived class for C5 number engine-related exceptions.
+ */
+class C5NumberEngineException: public C5Exceptions {
   public:
-    enum exceptionCodes {AT_OPEN};
+    enum exceptionCodes { AT_OPEN };
     C5NumberEngineException(exceptionCodes code);
-    const char* where() const override{return "C5NumberEngine";}
+    const char* where() const override { return "C5NumberEngine"; }
 };
 
-class ServerException: public C5Exceptions{
+/**
+ * @brief Derived class for server-related exceptions.
+ */
+class ServerException: public C5Exceptions {
   public:
-    enum exceptionCodes {INV_SYN};
+    enum exceptionCodes { INV_SYN };
     ServerException(exceptionCodes code);
-    const char* where() const override{return "Server";}
+    const char* where() const override { return "Server"; }
 };
+
 #endif
