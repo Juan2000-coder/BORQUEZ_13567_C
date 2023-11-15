6548be79fe4a495fb9f67b3b9934221f5718acdb
Juan <borquez.juan00@gmail.com>
2023-11-15 09:45:17 -0300

    C5NumberEngine.t.hpp

--- cons_5/export/include/server/C5NumberEngine.h
+++ cons_5/export/include/server/C5NumberEngine.h
@@ -8,27 +8,92 @@
 #include <vector>
 #include <fstream>
 
-
-class C5NumberEngine{
-  public:
-    //constructor
+/**
+ * @brief The C5NumberEngine class manages the generation of the numbers,
+ *        user validation, and retrieval of statistical information.
+ */
+class C5NumberEngine {
+public:
+    /**
+     * @brief Default constructor for C5NumberEngine.
+     */
     C5NumberEngine();
 
-    //methods
+    /**
+     * @brief Validates a user based on their user ID.
+     *
+     * @param userId The user ID to validate.
+     * @return True if the user is valid; otherwise, false.
+     */
     bool userValidate(int userId);
-    
-    int getInt(int bmin, int bmax);
-    int getInt();
-    double getReal(double bmin, double bmax);
-    double getReal();
-    template<typename Type> const C5Number<Type> &getNumberOld(int ordinal) const;//esto también podría ser un string
-    Stats getStat() const;//esto puede ser una nueva clase de estadística
-    const C5Requirement &getNumberList() const;//me parece que con ese tipo ocupa menos espacio
 
-  private:
-    std::vector<C5Requirement> requirements;
-    std::string validUsersFile;
-    int requirementIndex;
+    /**
+     * @brief Generates a random integer within the specified range.
+     *
+     * @param bmin The minimum value of the range.
+     * @param bmax The maximum value of the range.
+     * @return A randomly generated integer.
+     */
+    int getInt(int bmin, int bmax);
+
+    /**
+     * @brief Generates a random integer when no boundary is specified.
+
+     * If there is a previous number of the same kind, it takes the boundaries of that number
+     * to generate the new number; otherwise, it throws an exception.
+     * @return A randomly generated integer.
+     */
+    int getInt();
+
+    /**
+     * @brief Generates a random real number within the specified range.
+     *
+     * @param bmin The minimum value of the range.
+     * @param bmax The maximum value of the range.
+     * @return A randomly generated real number.
+     */
+    double getReal(double bmin, double bmax);
+
+    /**
+     * @brief Generates a random real number.
+
+     * If there is a previous number of the same kind, it takes the boundaries of that number
+     * to generate the new number; otherwise, it throws an exception.
+     * @return A randomly generated real number.
+     */
+    double getReal();
+
+    /**
+     * @brief Retrieves an old C5Number object based on its ordinal.
+     *
+     * @tparam Type The type of the C5Number object (int or double).
+     * @param ordinal The ordinal of the C5Number object to retrieve.
+     * @return A constant reference to the retrieved C5Number object.
+     */
+    template <typename Type>
+    const C5Number<Type> &getNumberOld(int ordinal) const;
+
+    /**
+     * @brief Retrieves basic statistical information containing
+     * the number of numbers generated for a specified requirement
+     * the sum of the numbers and the mean of the numbers.
+     *
+     * @return A Stats object containing statistical information.
+     */
+    Stats getStat() const;
+
+    /**
+     * @brief Retrieves the list of numbers for a requirement.
+     *
+     * @return A constant reference to the C5Requirement object representing a requirement.
+     */
+    const C5Requirement &getNumberList() const;
+
+private:
+    std::vector<C5Requirement> requirements; ///< Vector to store requirements.
+    std::string validUsersFile; ///< File path for storing valid user information.
+    int requirementIndex; ///< Index to keep track the current requirement.
 };
+
 #include "C5NumberEngine.t.hpp"
 #endif
