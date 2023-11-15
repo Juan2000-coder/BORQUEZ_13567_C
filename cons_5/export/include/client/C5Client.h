723883b5f19872346989380d951bf6cf77b96019
Juan <borquez.juan00@gmail.com>
2023-11-15 01:32:33 -0300

    documentación C5Client.h

--- cons_5/export/include/client/C5Client.h
+++ cons_5/export/include/client/C5Client.h
@@ -1,18 +1,44 @@
 #ifndef _C5CLIENT_H
 #define _C5CLIENT_H
+
 #include "XmlRpc.h"
 #include <string>
 #include <fstream>
 
 using namespace XmlRpc;
 
-class C5Client: public XmlRpcClient{
-    public:
-        using XmlRpcClient::XmlRpcClient;
-        bool parseResponse(XmlRpcValue& result) override;
-        void setXmlFile(std::string file);
-        bool dumToFile(std::string);
-    private:
-        std::string XmlFile;
+/**
+ * @brief Inheritance specifically designed to override the parseResponse method
+ * in the base class to dump the responses in XML format to a file.
+ */
+class C5Client : public XmlRpcClient {
+public:
+    using XmlRpcClient::XmlRpcClient;
+
+    /**
+     * @brief Same implementation in base class.
+     * But also dumps the response in XML format to the Xmlfile.
+     */
+    bool parseResponse(XmlRpcValue& result) override;
+
+    /**
+     * @brief Sets the file to which the XML responses will be dumped.
+     * @param file The name of the XML file.
+     */
+    void setXmlFile(std::string file);
+
+    /**
+     * @brief Dumps the content to XmlFile.
+     *
+     * Dumps the content of the response.
+     *
+     * @param file The path to the file where the content will be dumped.
+     * @return True if the dump operation is successful, false otherwise.
+     */
+    bool dumToFile(std::string response);
+
+private:
+    std::string XmlFile; /**< The path to the XML file  */
 };
+
 #endif
