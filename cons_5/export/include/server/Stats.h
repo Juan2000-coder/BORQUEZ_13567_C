f4a8f50fa4dec83856b094e811173be6e809830f
Juan <borquez.juan00@gmail.com>
2023-11-15 10:22:32 -0300

    Stats.h doc

--- cons_5/export/include/server/Stats.h
+++ cons_5/export/include/server/Stats.h
@@ -1,20 +1,71 @@
 #ifndef _STATS_H
 #define _STATS_H
 
-class Stats{
-    public:
-        Stats(double _sum, double _mean, int _count):sum(_sum), mean(_mean), count(_count){};
-        void setSum(double sum);
-        void setMean(double mean);
-        void setCount(int count);
+/**
+ * @brief Represents statistical information including sum, mean, and count.
+ *
+ * This class encapsulates statistical information such as sum, mean, and count.
+ * It provides methods to set and retrieve these statistical values.
+ */
+class Stats {
+public:
+    /**
+     * @brief Constructor to initialize statistical values.
+     *
+     * Initializes the Stats object with the provided sum, mean, and count.
+     *
+     * @param _sum   The sum value for the statistical information.
+     * @param _mean  The mean value for the statistical information.
+     * @param _count The count value for the statistical information.
+     */
+    Stats(double _sum, double _mean, int _count);
 
-        double getSum() const;
-        double getMean() const;
-        int getCount() const;
-        
-    private:
-        double sum;
-        double mean;
-        int count;
+    /**
+     * @brief Sets the sum value in the statistical information.
+     *
+     * @param sum The sum value to be set.
+     */
+    void setSum(double sum);
+
+    /**
+     * @brief Sets the mean value in the statistical information.
+     *
+     * @param mean The mean value to be set.
+     */
+    void setMean(double mean);
+
+    /**
+     * @brief Sets the count value in the statistical information.
+     *
+     * @param count The count value to be set.
+     */
+    void setCount(int count);
+
+    /**
+     * @brief Retrieves the sum value from the statistical information.
+     *
+     * @return The sum value.
+     */
+    double getSum() const;
+
+    /**
+     * @brief Retrieves the mean value from the statistical information.
+     *
+     * @return The mean value.
+     */
+    double getMean() const;
+
+    /**
+     * @brief Retrieves the count value from the statistical information.
+     *
+     * @return The count value.
+     */
+    int getCount() const;
+
+private:
+    double sum;   ///< The sum value in the statistical information.
+    double mean;  ///< The mean value in the statistical information.
+    int count;    ///< The count value in the statistical information.
 };
+
 #endif
