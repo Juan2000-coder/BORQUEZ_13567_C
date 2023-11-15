52f2bb0360528d22cdd734929de7fadfccf1faed
Juan <borquez.juan00@gmail.com>
2023-11-15 15:07:31 -0300

    C5NumberEngine.t.hpp doc header

--- cons_5/export/include/server/C5NumberEngine.t.hpp
+++ cons_5/export/include/server/C5NumberEngine.t.hpp
@@ -1,3 +1,15 @@
+/**
+ * @file    C5NumberEngine.t.hpp
+ * @brief   Template implementation file for C5NumberEngine class member functions.
+ * 
+ * @details This file contains the template implementation for the C5NumberEngine class's
+ *          getNumberOld member function, which retrieves an old C5Number object based on its ordinal.
+ * 
+ * @version 1.0
+ * @date    2023-11-15
+ * @author  Juan Manuel Borquez
+ * @contact borquez.juan00@gmail.com
+ */
 #include "C5NumberEngine.h"
 
 template<typename Type>
