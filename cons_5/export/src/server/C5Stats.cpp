0dd1f43c76be11068ca0ec5c2ff17be3f9aaacde
Juan <borquez.juan00@gmail.com>
2023-11-15 14:12:42 -0300

    doc header in C5Stats.cpp

--- cons_5/export/src/server/C5Stats.cpp
+++ cons_5/export/src/server/C5Stats.cpp
@@ -1,3 +1,15 @@
+/**
+ * @file     C5Stats.cpp
+ * @brief    Implementation of the C5Stats class.
+ * 
+ * @details  Implements the methods of the C5Stats class.
+ * 
+ * @version  1.0
+ * @date     2023-11-15
+ * @author   Juan Manuel Borquez
+ * @contact  borquez.juan00@gmail.com
+ */
+
 #include "C5Stats.h"
 
 // Setters
